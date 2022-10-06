813e0d4861edbbb349ac3104de3c8f4e26f1b019
Be-student <presentsong@naver.com>
2022-10-06 16:20:49 +0900

    feat(domain): add Audience class

---
+++ app/src/main/java/objects/ch1/Audience.java
@@ -0,0 +1,12 @@
+package objects.ch1;
+
+public class Audience {
+    private Bag bag;
+    public Audience(Bag bag){
+        this.bag=bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}

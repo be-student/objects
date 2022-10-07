94f292a1b4f7b3c9006576e347105e0e1bb37567
Be-student <presentsong@naver.com>
2022-10-07 15:35:00 +0900

    feat(domain): add discount policies

---
+++ app/src/main/java/objects/ch2/NoneDiscountPolicy.java
@@ -0,0 +1,8 @@
+package objects.ch2;
+
+public class NoneDiscountPolicy extends DiscountPolicy{
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
